78482a6dd2f08e48247bd2214bdca03d106ad02c
mckennapaul27 <mckennapaul27@gmail.com>
2017-10-09 16:21:23 +0100

    message

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Track from './Track';
+import 'TrackList.css';
+
+class TrackList extends React.component {
+  render() {
+    return (
+      <div className="TrackList">
+    //<!-- You will add a map method that renders a set of Track components  -->
+      </div>
+    );
+  }
+};
+
+export TrackList;
